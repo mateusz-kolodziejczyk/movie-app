c3a77daeb879469468328347bba6d929d4c4722e
Mateusz Kolodziejczyk <mateuszlk1998@gmail.com>
2020-11-16 12:11:35 +0000

    Full review page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import MovieReviewPage from "./pages/movieReviewPage";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'   
@@ -26,6 +27,7 @@ const App = () => {
         <Switch>
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
+          <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route path="/" component={HomePage} />
           <Redirect from="*" to="/" />
         </Switch>
