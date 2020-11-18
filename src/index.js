38fbed183c9254141081407ab5244f2e4a14a06b
Mateusz Kolodziejczyk <mateuszlk1998@gmail.com>
2020-11-18 11:49:26 +0000

    Refactor: Movie list and genre list now stored in contexts

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link} from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import MovieReviewPage from "./pages/movieReviewPage";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
-import FavoriteMoviesPage from './pages/favoritesMoviesPage'   
-import UpcomingMoviesPage from './pages/upcomingMoviesPage'   
+import FavoriteMoviesPage from './pages/favoritesMoviesPage'
+import UpcomingMoviesPage from './pages/upcomingMoviesPage'
 import SiteHeader from './components/siteHeader'
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="jumbotron">
-      <SiteHeader />      {/* New Header  */}
-      <div className="container-fluid">
-        <Switch>
-          <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
-          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
-          <Route path="/movies/:id" component={MoviePage} />
-          <Route path="/reviews/:id" component={MovieReviewPage} />
-          <Route path="/" component={HomePage} />
-          <Redirect from="*" to="/" />
-        </Switch>
+      <div className="jumbotron">
+        <SiteHeader />      {/* New Header  */}
+        <div className="container-fluid">
+          <MoviesContextProvider>
+            <GenresContextProvider>
+              <Switch>
+                <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+                <Route path="/movies/:id" component={MoviePage} />
+                <Route path="/reviews/:id" component={MovieReviewPage} />
+                <Route path="/" component={HomePage} />
+                <Redirect from="*" to="/" />
+              </Switch>
+            </GenresContextProvider>
+          </MoviesContextProvider>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 };
 
