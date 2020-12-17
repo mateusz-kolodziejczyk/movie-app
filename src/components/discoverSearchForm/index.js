fe26943f172b9ddd71611a52216d31695b21bcf7
Mateusz Kolodziejczyk <mateuszlk1998@gmail.com>
2020-12-17 16:52:43 +0000

    Added minimum rating and minimum votes settings to search

--- src/components/discoverSearchForm/index.js
+++ src/components/discoverSearchForm/index.js
@@ -20,7 +20,9 @@ const DiscoverSearchForm = ({ history }) => {
         const sortString = data.sort_by + "." + data.sort_order;
         const queryString = "?sort_by=" + sortString + "&include_adult=" + data.include_adult
             + (data.year !== "" ? "&year=" + data.year : "")
-            + (data.with_genres !== "0" ? "&with_genres=" + data.with_genres : "");
+            + (data.with_genres !== "0" ? "&with_genres=" + data.with_genres : "")
+            + (data.vote_average !== "" ? "&vote_average.gte=" + data.vote_average : "")
+            + (data.vote_count !== "" ? "&vote_count.gte=" + data.vote_count : "")
         console.log(data);
         console.log(queryString);
         history.push("/search" + queryString);
@@ -73,7 +75,7 @@ const DiscoverSearchForm = ({ history }) => {
                         />
                     </label>
                 </div>
-                <div className="form-group col-md-8">
+                <div className="form-group col-md-3">
                     <label>Genre
                            <select name="with_genres" class="form-control" ref={register}>
                             {context.genres.map(genre => {
@@ -88,28 +90,55 @@ const DiscoverSearchForm = ({ history }) => {
                         </select>
                     </label>
                 </div>
+
+            </div>
+            <div class="form-row">
+                <div className="form-group col-md-3">
+                    <label>Minimum Rating
+                    <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Rating"
+                            defaultValue={""}
+                            name="vote_average"
+                            ref={register}
+                        />
+                    </label>
+                </div>
+                <div className="form-group col-md-3">
+                    <label>Minimum No. of Votes
+                    <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Votes"
+                            defaultValue={"50"}
+                            name="vote_count"
+                            ref={register}
+                        />
+                    </label>
+                </div>
             </div>
             <div class="form-row">
 
                 <div class="form-group col-md-3">
-                <button type="submit" className="btn btn-primary">
-                    Submit
-      </button>
-      </div>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                </button>
+                </div>
                 <div class="form-group col-md-3">
-                <button
-                    type="reset"
-                    className="btn btn-primary reset"
-                    onClick={() => {
-                        reset({
-                            author: "",
-                            content: ""
-                        });
-                    }}
-                >
-                    Reset
+                    <button
+                        type="reset"
+                        className="btn btn-primary reset"
+                        onClick={() => {
+                            reset({
+                                author: "",
+                                content: ""
+                            });
+                        }}
+                    >
+                        Reset
       </button>
-      </div>
+                </div>
             </div>
         </form>
     );
